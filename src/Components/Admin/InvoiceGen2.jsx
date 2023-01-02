ce63a6dd35165a2bcd99124532a076757b9badb3
maltamimi96 <46971446+maltamimi96@users.noreply.github.com>
2023-01-02 20:45:08 +1100

    check point almost everything works

--- src/Components/Admin/InvoiceGen2.jsx
+++ src/Components/Admin/InvoiceGen2.jsx
@@ -8,6 +8,8 @@ function InvoiceGen2() {
     customerName: "",
     invoiceNumber: "",
     invoiceDate: "",
+    dueDate: "",
+
     notes: "",
     items: [],
     total: 0,
@@ -53,67 +55,105 @@ function InvoiceGen2() {
     setFormData({ ...formData, total })
   }, [formData.items])
   console.log(formData)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addDocument("invoices", formData)
+      .then(() => {
+        toast("Article Added Succesfuly")
+      })
+      .catch((error) => toast(error))
+  }
   return (
     <section className="bg-white shadow-md rounded px-20 pt-20 pb-8 mb-4 min-h-screen ">
       <div className="container mx-auto p-4">
-        <div className="flex items-center justify-between py-4 font-bold border-b border-gray-200">
-          <div className="text-center mb-5">
-            <div
-              className="flex justify-between flex-wrap
+        <div className="text-center mb-5">
+          <div
+            className="flex justify-between flex-wrap
         ">
-              <img
-                src={
-                  "https://firebasestorage.googleapis.com/v0/b/msa-mowing.appspot.com/o/logo-sm.png?alt=media&token=fa8cffca-f2c1-435f-9e5d-9273eb72513c"
-                }
-                alt="Logo"
-                className="h-12 mb-4"
-              />
-              <h1 className="text-6xl uppercase">Invoice</h1>
-            </div>
-
-            <h4 className="text-left font-semibold">ABN : 73 882 493 738 </h4>
-            <h4 className="text-left font-semibold">
-              Email : msamowing@gmail.com
-            </h4>
-            <h4 className="text-left font-semibold">Phone : +61 493 498 074</h4>
+            <img
+              src={
+                "https://firebasestorage.googleapis.com/v0/b/msa-mowing.appspot.com/o/logo-sm.png?alt=media&token=fa8cffca-f2c1-435f-9e5d-9273eb72513c"
+              }
+              alt="Logo"
+              className="h-12 mb-4"
+            />
+            <h1 className="text-6xl uppercase">Invoice</h1>
           </div>
-          <div>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="text"
-              name="customerName"
-              placeholder="Customer Name"
-              value={formData.customerName}
-              onChange={handleChange}
-            />
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="text"
-              name="invoiceNumber"
-              placeholder="Invoice #"
-              value={formData.invoiceNumber}
-              onChange={handleChange}
-            />
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="date"
-              name="invoiceDate"
-              placeholder="Invoice Date"
-              value={formData.invoiceDate}
-              onChange={handleChange}
-            />
+
+          <h4 className="text-left font-semibold">ABN : 73 882 493 738 </h4>
+          <h4 className="text-left font-semibold">
+            Email : msamowing@gmail.com
+          </h4>
+          <h4 className="text-left font-semibold">Phone : +61 493 498 074</h4>
+        </div>
+        <div className="flex items-center justify-between py-4 font-bold border-b border-gray-200">
+          <div className="flex flex-col md:flex-row md:gap-20">
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="client-name">
+                Client Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="customerName"
+                placeholder="Customer Name"
+                value={formData.customerName}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="client-name">
+                Invoice Number
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="invoiceNumber"
+                placeholder="Invoice #"
+                value={formData.invoiceNumber}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="client-name">
+                Invoice Date
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="date"
+                name="invoiceDate"
+                placeholder="Invoice Date"
+                value={formData.invoiceDate}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="client-name">
+                Due date
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="date"
+                name="dueDate"
+                placeholder="Invoice Date"
+                value={formData.dueDate}
+                onChange={handleChange}
+              />
+            </div>
           </div>
         </div>
-        <div className="py-4 border-b border-gray-200">
-          <textarea
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            name="notes"
-            placeholder="Notes"
-            value={formData.notes}
-            onChange={handleChange}
-          />
+        <div className="py-4  border-b border-gray-200 my-20">
           <table className="w-full">
-            <thead>
+            <thead className="bg-gray-300 rounded">
               <tr>
                 <th className="px-4 py-2">Item</th>
                 <th className="px-4 py-2">Price</th>
@@ -122,7 +162,8 @@ function InvoiceGen2() {
                 <th className="px-4 py-2"></th>
               </tr>
             </thead>
-            <tbody>
+
+            <tbody className="">
               {formData.items.map((item, index) => (
                 <tr key={index}>
                   <td className="px-4 py-2">
@@ -155,28 +196,44 @@ function InvoiceGen2() {
                   <td className="px-4 py-2">{item.price * item.quantity}</td>
                   <td className="px-4 py-2">
                     <button
-                      className="btn btn-link"
+                      className="btn btn-primary uppercase text-red-600 font-semibold text-1xl   p-4 rounded md:text-sm"
                       onClick={() => handleRemoveItem(index)}>
                       Remove
                     </button>
                   </td>
                 </tr>
               ))}
-              <tr>
-                <td colSpan="3" className="px-4 py-2 font-bold">
-                  Total
-                </td>
-                <td className="px-4 py-2 font-bold">{formData.total}</td>
-              </tr>
             </tbody>
           </table>
         </div>
         <div className="py-4 flex justify-end">
-          <button className="btn" onClick={handleAddItem}>
-            Add Item
+          <button
+            className="btn btn-primary mt-4 text-emerald-600 font-semibold"
+            onClick={handleAddItem}>
+            + Add Item
           </button>
         </div>
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          for="client-name">
+          Notes
+        </label>
+        <textarea
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          name="notes"
+          placeholder="Payment details etc..."
+          rows={10}
+          value={formData.notes}
+          onChange={handleChange}
+        />
+        <h4>{formData.total}</h4>
       </div>
+      <button
+        className="btn btn-primary font-semibold mt-4 bg-sky-400 rounded-full px-6 py-2 "
+        onChange={handleSubmit}>
+        Generate
+      </button>
+      <ToastContainer />
     </section>
   )
 }
